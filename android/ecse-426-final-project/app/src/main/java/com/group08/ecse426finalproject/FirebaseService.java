4a1c31dadc4ee72cf53c6ff70e5bdf377dbc0d34
Sean Stappas <seanstappas@gmail.com>
2018-03-26 20:48:55 -0400

    added firebase with simple bytes upload

---
+++ android/ecse-426-final-project/app/src/main/java/com/group08/ecse426finalproject/FirebaseService.java
@@ -0,0 +1,40 @@
+package com.group08.ecse426finalproject;
+
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class FirebaseService {
+    private static final String TAG = "FirebaseService";
+    private StorageReference storageRef;
+
+    public FirebaseService()
+    {
+        storageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    public void uploadBytes(byte[] bytes, String path)
+    {
+        storageRef.child(path).putBytes(bytes).addOnSuccessListener(
+                new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Log.d(TAG, "Uploaded URL: " + downloadUrl);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Upload failed");
+                    }
+        });
+    }
+}
