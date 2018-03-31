076c679aa032c85419e965845dc6f7990d52eabd
Sean Stappas <seanstappas@gmail.com>
2018-03-31 14:33:44 -0400

    uncommented speech service demo call

--- android/ecse-426-final-project/app/src/main/java/com/group08/ecse426finalproject/MainActivity.java
+++ android/ecse-426-final-project/app/src/main/java/com/group08/ecse426finalproject/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         new FirebaseService().uploadBytes(
                 resourceAccess.readRawResourceBytes(R.raw.audio),
                 "audio/demo.raw");
-        //new SpeechService(this, new BluetoothTransmitter(), resourceAccess).sendDemoRequestBytes();
+        new SpeechService(this, new BluetoothTransmitter(), resourceAccess).sendDemoRequestBytes();
     }
 }
