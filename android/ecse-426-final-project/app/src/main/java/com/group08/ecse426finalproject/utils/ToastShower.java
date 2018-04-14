baa5c0aefb210efc84b30b15c0b69d2532889274
Sean Stappas <seanstappas@gmail.com>
2018-04-14 16:29:04 -0400

    modified toast y position

--- android/ecse-426-final-project/app/src/main/java/com/group08/ecse426finalproject/utils/ToastShower.java
+++ android/ecse-426-final-project/app/src/main/java/com/group08/ecse426finalproject/utils/ToastShower.java
@@ -8,7 +8,7 @@ import android.widget.Toast;
 public class ToastShower {
     public static void showToast(Context context, String string) {
         Toast toast = Toast.makeText(context, string, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER | Gravity.BOTTOM, 0, 0);
+        toast.setGravity(Gravity.CENTER | Gravity.BOTTOM, 0, 200);
         toast.show();
     }
 }
